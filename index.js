ee0119ac66301df41d349e345d5bc6f15d8b3a49
Lashanique <lcars34@wgu.edu>
2023-04-13 20:03:55 -0400

    functions lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(streetNumber) {
+    const hqStreetNumber = 42;
+    let distanceInBlocks = Math.abs(streetNumber - hqStreetNumber);
+    return distanceInBlocks;
+  }
+
+
+  function distanceFromHqInFeet(streetNumber) {
+    const blockLength = 264; 
+    const distanceInBlocks = Math.abs(streetNumber - 42);
+    const distanceInFeet = distanceInBlocks * blockLength;
+    return distanceInFeet;
+  }
+  function distanceTravelledInFeet(start, destination) {
+    const blockLength = 264; 
+    const distanceInBlocks = Math.abs(destination - start);
+    const distanceInFeet = distanceInBlocks * blockLength;
+    return distanceInFeet;
+  }
+  
+  function calculatesFarePrice(start, destination) {
+    const distanceInFeet = distanceTravelledInFeet(start, destination);
+  
+    if (distanceInFeet <= 400) {
+      return 0;
+    } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+      const farePrice = (distanceInFeet - 400) * 0.02;
+      return farePrice;
+    } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+      return 25;
+    } else {
+      return 'cannot travel that far';
+    }
+  }
+  
